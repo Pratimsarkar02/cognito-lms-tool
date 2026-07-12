15d29080170df1ca25a1572ae5dbb0d8ec1f1369
Pratim <pbbubun3@gmail.com>
2026-07-13 03:20:55 +0530

    temporarily origin hardcoded

--- backend/server.js
+++ backend/server.js
@@ -38,17 +38,19 @@ const allowedOrigins = [
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: allowedOrigins, 
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: 'https://cognito-lms-tool-frontend.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials:true
+ }
+));
 
 const io = new Server(httpServer,  {
     cors: {
-        origin: allowedOrigins,
-        methods:["GET", "POST"],
+        origin: 'https://cognito-lms-tool-frontend.onrender.com',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
     },
 });
